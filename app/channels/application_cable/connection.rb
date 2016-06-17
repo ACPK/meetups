b34439101b5c460cd07e8e4d536b846ee3eb333e
Peter <pc_cfa@runbox.com>
2016-06-17 22:30:29 +1000

    added current_user tracking to Connection class

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,10 +1,26 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :uuid
+    identified_by :current_user
 
     def connect
-      self.uuid = SecureRandom.uuid
+      self.current_user = find_verified_user
+      logger.add_tags current_user.username
     end
+
+    def disconnect
+      # Any cleanup work needed when the cable connection is cut.
+    end
+
+    protected
+      def find_verified_user
+       #if current_user = User.find_by_identity cookies.signed[:identity_id]
+        if current_user = env['warden'].user
+          current_user
+        else
+          #reject_unauthorized_connection
+        end
+      end
   end
+
 end
